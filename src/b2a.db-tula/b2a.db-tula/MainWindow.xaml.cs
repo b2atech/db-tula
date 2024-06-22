b82df6d10d4914be69345cb68dd8dce5740ead77
Bharat <bharat.mane@gmail.com>
2024-06-22 09:44:13 +0530

    fix: issues with comparision

--- src/b2a.db-tula/b2a.db-tula/MainWindow.xaml.cs
+++ src/b2a.db-tula/b2a.db-tula/MainWindow.xaml.cs
@@ -68,7 +68,7 @@ namespace b2a.db_tula
             var sourceTables = sourceSchemaFetcher.GetTables();
             var targetTables = targetSchemaFetcher.GetTables();
             var schemaComparer = new SchemaComparer();
-            var tableDifferences = schemaComparer.CompareTables(sourceTables, targetTables);
+            var tableDifferences = schemaComparer.CompareTables(sourceTables, targetTables, sourceConnectionString, targetConnectionString);
             LogToConsole("Table comparison completed.");
 
             var functionDifferences = schemaComparer.CompareFunctions(sourceSchemaFetcher.GetFunctions(), targetSchemaFetcher.GetFunctions(), sourceSchemaFetcher, targetSchemaFetcher);
@@ -89,7 +89,7 @@ namespace b2a.db_tula
                 var selectedComparison = savedComparisons.First(c => c.Name == ComparisonComboBox.SelectedItem.ToString());
 
                 string sourceConnectionString = selectedComparison.SourceConnectionString;
-                string targetConnectionString = selectedComparison.TargetConnectionString;
+                string targetConnectionString = EncryptionHelper.DecryptString(selectedComparison.TargetConnectionString).Replace("&amp", "&");
 
                 var sourceConnection = new DatabaseConnection(sourceConnectionString);
                 var targetConnection = new DatabaseConnection(targetConnectionString);
@@ -97,78 +97,78 @@ namespace b2a.db_tula
                 var sourceSchemaFetcher = new SchemaFetcher(sourceConnection, LogToConsole);
                 var targetSchemaFetcher = new SchemaFetcher(targetConnection, LogToConsole);
 
-                var detailsData = new List<DetailsResult>();
+                var detailsData = new List<ColumnComparisonResult>();
                 var schemaComparer = new SchemaComparer();
 
                 if (selectedItem.Type == "Table")
                 {
                     DetailsGrid.Visibility = Visibility.Visible;
                     CodeDisplayGrid.Visibility = Visibility.Collapsed;
+                    detailsData = selectedItem.ColumnComparisonResults;
+                    //var sourceColumns = sourceSchemaFetcher.GetColumns(selectedItem.SourceName);
+                    //var targetColumns = targetSchemaFetcher.GetColumns(selectedItem.DestinationName);
+                    ////var columnDifferences = schemaComparer.CompareColumns(sourceColumns, targetColumns);
 
-                    var sourceColumns = sourceSchemaFetcher.GetColumns(selectedItem.SourceName);
-                    var targetColumns = targetSchemaFetcher.GetColumns(selectedItem.DestinationName);
-                    var columnDifferences = schemaComparer.CompareColumns(sourceColumns, targetColumns);
+                    ////foreach (var column in columnDifferences)
+                    ////{
+                    ////    detailsData.Add(new DetailsResult
+                    ////    {
+                    ////        SourceName = column.SourceName,
+                    ////        SourceType = column.SourceType,
+                    ////        SourceLength = column.SourceLength,
+                    ////        DestinationName = column.DestinationName,
+                    ////        DestinationType = column.DestinationType,
+                    ////        DestinationLength = column.DestinationLength,
+                    ////        Comparison = column.Comparison
+                    ////    });
+                    ////}
 
-                    foreach (var column in columnDifferences)
-                    {
-                        detailsData.Add(new DetailsResult
-                        {
-                            SourceName = column.SourceName,
-                            SourceType = column.SourceType,
-                            SourceLength = column.SourceLength,
-                            DestinationName = column.DestinationName,
-                            DestinationType = column.DestinationType,
-                            DestinationLength = column.DestinationLength,
-                            Comparison = column.Comparison
-                        });
-                    }
+                    //var sourcePrimaryKeys = sourceSchemaFetcher.GetPrimaryKeys(selectedItem.SourceName);
+                    //var targetPrimaryKeys = targetSchemaFetcher.GetPrimaryKeys(selectedItem.DestinationName);
+                    //var primaryKeyDifferences = schemaComparer.ComparePrimaryKeys(sourcePrimaryKeys, targetPrimaryKeys);
 
-                    var sourcePrimaryKeys = sourceSchemaFetcher.GetPrimaryKeys(selectedItem.SourceName);
-                    var targetPrimaryKeys = targetSchemaFetcher.GetPrimaryKeys(selectedItem.DestinationName);
-                    var primaryKeyDifferences = schemaComparer.ComparePrimaryKeys(sourcePrimaryKeys, targetPrimaryKeys);
+                    //foreach (var key in primaryKeyDifferences)
+                    //{
+                    //    detailsData.Add(new DetailsResult
+                    //    {
+                    //        SourceName = key.SourceName,
+                    //        SourceType = "PrimaryKey",
+                    //        DestinationName = key.DestinationName,
+                    //        DestinationType = "PrimaryKey",
+                    //        Comparison = key.Comparison
+                    //    });
+                    //}
 
-                    foreach (var key in primaryKeyDifferences)
-                    {
-                        detailsData.Add(new DetailsResult
-                        {
-                            SourceName = key.SourceName,
-                            SourceType = "PrimaryKey",
-                            DestinationName = key.DestinationName,
-                            DestinationType = "PrimaryKey",
-                            Comparison = key.Comparison
-                        });
-                    }
+                    //var sourceForeignKeys = sourceSchemaFetcher.GetForeignKeys(selectedItem.SourceName);
+                    //var targetForeignKeys = targetSchemaFetcher.GetForeignKeys(selectedItem.DestinationName);
+                    //var foreignKeyDifferences = schemaComparer.CompareForeignKeys(sourceForeignKeys, targetForeignKeys);
 
-                    var sourceForeignKeys = sourceSchemaFetcher.GetForeignKeys(selectedItem.SourceName);
-                    var targetForeignKeys = targetSchemaFetcher.GetForeignKeys(selectedItem.DestinationName);
-                    var foreignKeyDifferences = schemaComparer.CompareForeignKeys(sourceForeignKeys, targetForeignKeys);
-
-                    foreach (var key in foreignKeyDifferences)
-                    {
-                        detailsData.Add(new DetailsResult
-                        {
-                            SourceName = key.SourceName,
-                            SourceType = "ForeignKey",
-                            DestinationName = key.DestinationName,
-                            DestinationType = "ForeignKey",
-                            Comparison = key.Comparison
-                        });
-                    }
+                    //foreach (var key in foreignKeyDifferences)
+                    //{
+                    //    detailsData.Add(new DetailsResult
+                    //    {
+                    //        SourceName = key.SourceName,
+                    //        SourceType = "ForeignKey",
+                    //        DestinationName = key.DestinationName,
+                    //        DestinationType = "ForeignKey",
+                    //        Comparison = key.Comparison
+                    //    });
+                    //}
                 }
                 else if (selectedItem.Type == "Function" || selectedItem.Type == "Procedure")
                 {
                     DetailsGrid.Visibility = Visibility.Collapsed;
                     CodeDisplayGrid.Visibility = Visibility.Visible;
 
-                    string sourceCode = selectedItem.Type == "Function"
-                        ? sourceSchemaFetcher.GetFunctionDefinition(selectedItem.SourceName)
-                        : sourceSchemaFetcher.GetProcedureDefinition(selectedItem.SourceName);
+                    //string sourceCode = selectedItem.Type == "Function"
+                    //    ? sourceSchemaFetcher.GetFunctionDefinition(selectedItem.SourceName)
+                    //    : sourceSchemaFetcher.GetProcedureDefinition(selectedItem.SourceName);
 
-                    string targetCode = selectedItem.Type == "Function"
-                        ? targetSchemaFetcher.GetFunctionDefinition(selectedItem.DestinationName)
-                        : targetSchemaFetcher.GetProcedureDefinition(selectedItem.DestinationName);
+                    //string targetCode = selectedItem.Type == "Function"
+                    //    ? targetSchemaFetcher.GetFunctionDefinition(selectedItem.DestinationName)
+                    //    : targetSchemaFetcher.GetProcedureDefinition(selectedItem.DestinationName);
 
-                    SourceCodeTextBox.Text = sourceCode;
+                    SourceCodeTextBox.Text = selectedItem.;
                     TargetCodeTextBox.Text = targetCode;
                 }
 
@@ -184,14 +184,7 @@ namespace b2a.db_tula
         }
     }
 
-    public class ComparisonResult
-    {
-        public string Type { get; set; }
-        public string SourceName { get; set; }
-        public string DestinationName { get; set; }
-        public string Comparison { get; set; }
-    }
-
+    
     public class DetailsResult
     {
         public string SourceName { get; set; }
--- src/b2a.db-tula/b2a.db-tula/SchemaComparer.cs
+++ src/b2a.db-tula/b2a.db-tula/SchemaComparer.cs
@@ -1,49 +1,175 @@
 ﻿using System.Data;
 using System.Collections.Generic;
+using System;
+using System.Diagnostics;
 
 namespace b2a.db_tula
 {
     public class SchemaComparer
     {
-        // Existing methods for table, function, and procedure comparison
-
-        public List<ComparisonResult> CompareTables(DataTable sourceTables, DataTable targetTables)
+        public List<ComparisonResult> CompareTables(DataTable sourceTables, DataTable targetTables, string sourceConnectionString, string targetConnectionString)
         {
             var differences = new List<ComparisonResult>();
-
-            var sourceTableNames = new HashSet<string>();
-            foreach (DataRow row in sourceTables.Rows)
+            try
             {
-                sourceTableNames.Add(row["table_name"].ToString());
-            }
-
-            var targetTableNames = new HashSet<string>();
-            foreach (DataRow row in targetTables.Rows)
-            {
-                targetTableNames.Add(row["table_name"].ToString());
-            }
-
-            foreach (var table in sourceTableNames)
-            {
-                if (!targetTableNames.Contains(table))
+                var sourceTableNames = new HashSet<string>();
+                foreach (DataRow row in sourceTables.Rows)
                 {
-                    differences.Add(new ComparisonResult { Type = "Table", SourceName = table, DestinationName = table, Comparison = "Missing in Target" });
+                    sourceTableNames.Add(row["table_name"].ToString());
                 }
-                else
+
+                var targetTableNames = new HashSet<string>();
+                foreach (DataRow row in targetTables.Rows)
                 {
-                    differences.Add(new ComparisonResult { Type = "Table", SourceName = table, DestinationName = table, Comparison = "Matching" });
+                    targetTableNames.Add(row["table_name"].ToString());
+                }
+                foreach (var table in sourceTableNames)
+                {
+                    var currentTableComparision = new ComparisonResult { Type = "Table", SourceName = table, DestinationName = table };
+                    if (!targetTableNames.Contains(table))
+                    {
+                        currentTableComparision.Comparison = "Missing in Target";
+                       
+                    }
+                    else
+                    {
+                        
+                        // Compare columns
+                        var sourceColumns = GetTableColumns(sourceConnectionString, table);
+                        var targetColumns = GetTableColumns(targetConnectionString, table);
+                        var columnComparisonResults = CompareColumns(sourceColumns, targetColumns, out bool allColumnsMatch);
+                        currentTableComparision.Comparison = allColumnsMatch? "Matching" : "Not Matching";
+                        currentTableComparision.ColumnComparisonResults = columnComparisonResults;
+                    }
+                    differences.Add(currentTableComparision);
+                }
+
+                foreach (var table in targetTableNames)
+                {
+                    if (!sourceTableNames.Contains(table))
+                    {
+                        differences.Add(new ComparisonResult { Type = "Table", SourceName = table, DestinationName = table, Comparison = "Missing in Source" });
+                    }
                 }
             }
-
-            foreach (var table in targetTableNames)
+            catch (Exception ex)
             {
-                if (!sourceTableNames.Contains(table))
+                differences.Add(new ComparisonResult { Type = "Error", SourceName = "Table Comparison", DestinationName = "Table Comparison", Comparison = "Exception", Details = ex.Message });
+            }
+            return differences;
+        }
+
+        private DataTable GetTableColumns(string connectionString, string tableName)
+        {
+            using (var connection = new Npgsql.NpgsqlConnection(connectionString)) // PostgreSQL specific connection
+            {
+                connection.Open();
+                var query = $"SELECT column_name, data_type, character_maximum_length, is_nullable FROM information_schema.columns WHERE table_name = '{tableName}'";
+                using (var command = new Npgsql.NpgsqlCommand(query, connection))
                 {
-                    differences.Add(new ComparisonResult { Type = "Table", SourceName = table, DestinationName = table, Comparison = "Missing in Source" });
+                    using (var adapter = new Npgsql.NpgsqlDataAdapter(command))
+                    {
+                        var columns = new DataTable();
+                        adapter.Fill(columns);
+                        return columns;
+                    }
                 }
+            }
+        }
+
+        public List<ColumnComparisonResult> CompareColumns(DataTable sourceColumns, DataTable targetColumns, out bool allColumnsMatch)
+        {
+            var differences = new List<ColumnComparisonResult>();
+            allColumnsMatch = true;
+
+            try
+            {
+                var sourceColumnNames = new HashSet<string>();
+                foreach (DataRow row in sourceColumns.Rows)
+                {
+                    sourceColumnNames.Add(row["column_name"].ToString());
+                }
+
+                var targetColumnNames = new HashSet<string>();
+                foreach (DataRow row in targetColumns.Rows)
+                {
+                    targetColumnNames.Add(row["column_name"].ToString());
+                }
+
+                foreach (var column in sourceColumnNames)
+                {
+                    if (!targetColumnNames.Contains(column))
+                    {
+                        try
+                        {
+                            differences.Add(new ColumnComparisonResult { SourceName = column, DestinationName = column, SourceType = GetColumnValue(sourceColumns, column, "data_type"), DestinationType = "N/A", SourceLength = GetColumnValue(sourceColumns, column, "character_maximum_length"), DestinationLength = "N/A", Comparison = "Missing in Target" });
+
+                            allColumnsMatch = false;
+                        }
+                        catch(Exception ex)
+                        {
+                            Debug.WriteLine(ex);
+                        }
+                    }
+                    else
+                    {
+                        var sourceColumn = sourceColumns.AsEnumerable().First(r => r["column_name"].ToString() == column);
+                        var targetColumn = targetColumns.AsEnumerable().First(r => r["column_name"].ToString() == column);
+
+                        var sourceDataType = sourceColumn["data_type"].ToString();
+                        var targetDataType = targetColumn["data_type"].ToString();
+                        var sourceLength = sourceColumn["character_maximum_length"]?.ToString();
+                        var targetLength = targetColumn["character_maximum_length"]?.ToString();
+                        var sourceIsNullable = sourceColumn["is_nullable"].ToString();
+                        var targetIsNullable = targetColumn["is_nullable"].ToString();
+
+                        bool isTypeMatch = sourceDataType == targetDataType;
+                        bool isLengthMatch = sourceLength == targetLength;
+                        bool isNullableMatch = sourceIsNullable == targetIsNullable;
+
+                        string comparisonResult = (isTypeMatch && isLengthMatch && isNullableMatch) ? "Matching" : "Not Matching";
+
+                        if (comparisonResult == "Not Matching")
+                        {
+                            allColumnsMatch = false;
+                        }
+
+                        differences.Add(new ColumnComparisonResult
+                        {
+                            SourceName = column,
+                            DestinationName = column,
+                            SourceType = sourceDataType,
+                            DestinationType = targetDataType,
+                            SourceLength = sourceLength,
+                            DestinationLength = targetLength,
+                            SourceNullable = sourceIsNullable,
+                            DestinationNullable = targetIsNullable,
+                            Comparison = comparisonResult
+                        });
+                    }
+                }
+
+                foreach (var column in targetColumnNames)
+                {
+                    if (!sourceColumnNames.Contains(column))
+                    {
+                        differences.Add(new ColumnComparisonResult { SourceName = column, DestinationName = column, SourceType = GetColumnValue(sourceColumns, column, "data_type"), DestinationType = "N/A", SourceLength = GetColumnValue(sourceColumns, column, "character_maximum_length"), DestinationLength = "N/A", Comparison = "Missing in Source" });
+                        allColumnsMatch = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                differences.Add(new ColumnComparisonResult { SourceName = "Column Comparison", DestinationName = "Column Comparison", SourceType = "Exception", DestinationType = "Exception", Comparison = "Exception", Details = ex.Message });
+                allColumnsMatch = false;
             }
 
             return differences;
+        }
+        private string GetColumnValue(DataTable table, string columnName, string fieldName)
+        {
+            var row = table.AsEnumerable().FirstOrDefault(r => r["column_name"].ToString() == columnName);
+            return row?[fieldName]?.ToString() ?? "N/A";
         }
 
         public List<ComparisonResult> CompareFunctions(DataTable sourceFunctions, DataTable targetFunctions, SchemaFetcher sourceSchemaFetcher, SchemaFetcher targetSchemaFetcher)
@@ -130,50 +256,7 @@ namespace b2a.db_tula
             return differences;
         }
 
-        public List<ColumnComparisonResult> CompareColumns(DataTable sourceColumns, DataTable targetColumns)
-        {
-            var differences = new List<ColumnComparisonResult>();
-
-            var sourceColumnNames = new HashSet<string>();
-            foreach (DataRow row in sourceColumns.Rows)
-            {
-                sourceColumnNames.Add(row["column_name"].ToString());
-            }
-
-            var targetColumnNames = new HashSet<string>();
-            foreach (DataRow row in targetColumns.Rows)
-            {
-                targetColumnNames.Add(row["column_name"].ToString());
-            }
-
-            foreach (var column in sourceColumnNames)
-            {
-                if (!targetColumnNames.Contains(column))
-                {
-                    differences.Add(new ColumnComparisonResult { SourceName = column, DestinationName = column, SourceType = sourceColumns.Rows.Find(column)["data_type"].ToString(), DestinationType = "N/A", SourceLength = sourceColumns.Rows.Find(column)["character_maximum_length"].ToString(), DestinationLength = "N/A", Comparison = "Missing in Target" });
-                }
-                else
-                {
-                    var sourceColumn = sourceColumns.Rows.Find(column);
-                    var targetColumn = targetColumns.Rows.Find(column);
-                    bool isTypeMatch = sourceColumn["data_type"].ToString() == targetColumn["data_type"].ToString();
-                    bool isLengthMatch = sourceColumn["character_maximum_length"].ToString() == targetColumn["character_maximum_length"].ToString();
-                    string comparisonResult = (isTypeMatch && isLengthMatch) ? "Matching" : "Not Matching";
-                    differences.Add(new ColumnComparisonResult { SourceName = column, DestinationName = column, SourceType = sourceColumn["data_type"].ToString(), DestinationType = targetColumn["data_type"].ToString(), SourceLength = sourceColumn["character_maximum_length"].ToString(), DestinationLength = targetColumn["character_maximum_length"].ToString(), Comparison = comparisonResult });
-                }
-            }
-
-            foreach (var column in targetColumnNames)
-            {
-                if (!sourceColumnNames.Contains(column))
-                {
-                    differences.Add(new ColumnComparisonResult { SourceName = column, DestinationName = column, SourceType = "N/A", DestinationType = targetColumns.Rows.Find(column)["data_type"].ToString(), SourceLength = "N/A", DestinationLength = targetColumns.Rows.Find(column)["character_maximum_length"].ToString(), Comparison = "Missing in Source" });
-                }
-            }
-
-            return differences;
-        }
-
+      
         public List<KeyComparisonResult> ComparePrimaryKeys(DataTable sourcePrimaryKeys, DataTable targetPrimaryKeys)
         {
             var differences = new List<KeyComparisonResult>();
@@ -253,6 +336,16 @@ namespace b2a.db_tula
         }
     }
 
+    public class ComparisonResult
+    {
+        public string Type { get; set; }
+        public string SourceName { get; set; }
+        public string DestinationName { get; set; }
+        public string Comparison { get; set; }
+        public string Details { get; set; } // Additional details for errors or exceptions
+        public List<ColumnComparisonResult> ColumnComparisonResults { get; set; }
+    }
+
     public class ColumnComparisonResult
     {
         public string SourceName { get; set; }
@@ -261,7 +354,10 @@ namespace b2a.db_tula
         public string DestinationType { get; set; }
         public string SourceLength { get; set; }
         public string DestinationLength { get; set; }
+        public string SourceNullable { get; set; }
+        public string DestinationNullable { get; set; }
         public string Comparison { get; set; }
+        public string Details { get; set; } // Additional details for errors or exceptions
     }
 
     public class KeyComparisonResult
@@ -269,5 +365,6 @@ namespace b2a.db_tula
         public string SourceName { get; set; }
         public string DestinationName { get; set; }
         public string Comparison { get; set; }
+        public string Details { get; set; } // Additional details for errors or exceptions
     }
 }
